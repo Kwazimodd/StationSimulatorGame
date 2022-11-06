72ae9459ab90119560ddee9661a4a4296f32fc27
omisterdemono <dimon.zdrok.2002@gmail.com>
2022-11-06 18:16:19 +0200

    Client generator update, add proprties

--- src/main/java/ua/pz33/utils/configuration/PropertyRegistry.java
+++ src/main/java/ua/pz33/utils/configuration/PropertyRegistry.java
@@ -1,5 +1,10 @@
 package ua.pz33.utils.configuration;
 
+import java.awt.*;
+
 public final class PropertyRegistry {
     public static final String DUMB_ASS_SHIT_PROPERTY = "das";
+    public static final int MAX_AMOUNT_OF_CLIENTS = 20;
+    public static final Point ENTRANCE_POSITION = new Point(0,0);
+    public static final int CLIENT_SPAWN_RATE = 1;
 }
