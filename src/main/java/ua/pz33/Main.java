cb122339c0eb9f0cd21eb544e5b6ef6c09f4fb69
Maksym-Yurii Rudko <maksym-iurii.rudko.pz.2020@lpnu.ua>
2022-11-07 10:38:18 +0200

    Implemented clock

--- src/main/java/ua/pz33/Main.java
+++ src/main/java/ua/pz33/Main.java
@@ -4,6 +4,7 @@ import ua.pz33.registries.SpriteRegistry;
 import ua.pz33.sprites.DumbCircle;
 import ua.pz33.sprites.ImageSprite;
 import ua.pz33.timers.RenderTimer;
+import ua.pz33.utils.clock.GameClock;
 import ua.pz33.utils.configuration.ConfigurationMediator;
 import ua.pz33.utils.logs.LogMediator;
 
@@ -23,6 +24,8 @@ public class Main {
 
         // Invoke on Main thread
         EventQueue.invokeLater(Main::initializeSprites);
+
+        GameClock.getInstance().startTimer();
 
         LogMediator.getInstance().logMessage("Game started");
     }
