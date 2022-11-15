d1292539c66df3406f0978def95e167f17902864
Pavlo-Ivan Mykhalevych <pmykhalevych@tsukat.com>
2022-11-15 02:04:33 +0200

    Add station class with move queue method and its test

---
+++ src/main/java/ua/pz33/sprites/Entrance.java
@@ -0,0 +1,7 @@
+package ua.pz33.sprites;
+
+public class Entrance extends ImageSprite  {
+    public Entrance() {
+        super("Entrance200X200.png", 0);
+    }
+}
---
+++ src/main/java/ua/pz33/sprites/Exit.java
@@ -0,0 +1,7 @@
+package ua.pz33.sprites;
+
+public class Exit extends ImageSprite{
+    public Exit() {
+        super("Exit200X200.png", 0);
+    }
+}
