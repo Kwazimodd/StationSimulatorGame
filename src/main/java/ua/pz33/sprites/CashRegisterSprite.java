e0beb221652129c25461a779b15009b04a7c6c7b
funware <kosyk.nazar@gmail.com>
2022-11-16 17:53:24 +0200

    added cashregister sprite

---
+++ src/main/java/ua/pz33/sprites/CashRegisterSprite.java
@@ -0,0 +1,17 @@
+package ua.pz33.sprites;
+
+import java.awt.image.RescaleOp;
+
+public class CashRegisterSprite extends ImageSprite {
+    public CashRegisterSprite(String image) {
+        super(image);
+    }
+
+    public CashRegisterSprite(String image ,int zIndex) {
+        super(image, zIndex);
+    }
+
+    public void test(){
+
+    }
+}
