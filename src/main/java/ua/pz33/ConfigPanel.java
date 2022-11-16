4548e4f498f88b579fe6ce6d35f7f4db693ee529
Volodymyr <volodymyr.yurg@gmail.com>
2022-11-16 17:56:55 +0200

    pre-designed config view

--- src/main/java/ua/pz33/ConfigPanel.java
+++ src/main/java/ua/pz33/ConfigPanel.java
@@ -33,10 +33,63 @@ public class ConfigPanel extends JPanel {
         save.setAlignmentX(LEFT_ALIGNMENT);
         save.addActionListener(e -> configs().setValue(PropertyRegistry.TICKS_PER_CLIENT, Integer.parseInt(ticksPerClientField.getText())));
 
+        //Another configuration fields
+        //Time of service
+        JTextField serviceTimeField = new JTextField("10");
+        serviceTimeField.setAlignmentX(LEFT_ALIGNMENT);
+        serviceTimeField.setMaximumSize(new Dimension(Integer.MAX_VALUE, ticksPerClientField.getPreferredSize().height));
+        //Expected amount of people
+        JTextField expectedPeopleField = new JTextField("6");
+        expectedPeopleField.setAlignmentX(LEFT_ALIGNMENT);
+        expectedPeopleField.setMaximumSize(new Dimension(Integer.MAX_VALUE, ticksPerClientField.getPreferredSize().height));
+        //% of people in special group
+        JTextField percentSpecialField = new JTextField("5");
+        percentSpecialField.setAlignmentX(LEFT_ALIGNMENT);
+        percentSpecialField.setMaximumSize(new Dimension(Integer.MAX_VALUE, ticksPerClientField.getPreferredSize().height));
+
+
+        JTextField xCoordinateField = new JTextField("");
+        xCoordinateField.setAlignmentX(LEFT_ALIGNMENT);
+        xCoordinateField.setMaximumSize(new Dimension(Integer.MAX_VALUE, ticksPerClientField.getPreferredSize().height));
+
+        JTextField yCoordinateField = new JTextField("");
+        yCoordinateField.setAlignmentX(LEFT_ALIGNMENT);
+        yCoordinateField.setMaximumSize(new Dimension(Integer.MAX_VALUE, ticksPerClientField.getPreferredSize().height));
+
+        var addCashRegisterButton = new JButton("Cash Register");
+        addCashRegisterButton.setAlignmentX(LEFT_ALIGNMENT);
+        //addCashRegisterButton.addActionListener(e->);
+        var addReserveCashRegisterButton = new JButton("Reserve CR");
+        addReserveCashRegisterButton.setAlignmentX(LEFT_ALIGNMENT);
+        //addReserveCashRegisterButton.addActionListener();
+        var addEntranceButton = new JButton("Entrance");
+        addEntranceButton.setAlignmentX(LEFT_ALIGNMENT);
+        //addEntranceButton.addActionListener();
+
+
+        //configuration models
         add(configLabel);
         add(dasLabel);
         add(ticksPerClientField);
+        //another configuration components
+        add(new JLabel("Time of service for single CR"));
+        add(serviceTimeField);
+        add(new JLabel("Expected amount of people for single CR"));
+        add(expectedPeopleField);
+        add(new JLabel("Percent of special group peoples"));
+        add(percentSpecialField);
         add(save);
+        //spawning models components
+        add(new JLabel("Enter spawning cords:"));
+        add(new JLabel("x:"));
+        add(xCoordinateField);
+        add(new JLabel("y:"));
+        add(yCoordinateField);
+        add(new JLabel("Select creating model:"));
+        add(addEntranceButton);
+        add(addCashRegisterButton);
+        add(addReserveCashRegisterButton);
+
     }
 
     private ConfigurationMediator configs() {
