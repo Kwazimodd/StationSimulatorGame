44c1105420ea2e765a82d942bc0469dcbf63ebbd
funware <kosyk.nazar@gmail.com>
2022-11-17 02:03:46 +0200

    Refactoring states Exit state

--- src/main/java/ua/pz33/StationController.java
+++ src/main/java/ua/pz33/StationController.java
@@ -111,6 +111,15 @@ public class StationController {
         cashRegisterSprites.put(cashRegister.getId(), cashRegisterSprite);
     }
 
+    public void removeClient(Client client){
+        var clientSprite = clientSprites.get(client.getId());
+        SpriteRegistry.getInstance().removeSprite(clientSprite);
+
+        clientSprites.remove(clientSprite);
+        clients.remove(client);
+    }
+
+
     public Collection<CashRegister> getCashRegisters() {
         return cashRegisters.values();
     }
